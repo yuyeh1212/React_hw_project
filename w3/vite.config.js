44f16f3b21dc27c00313213a535e41554960c81a
yuyeh <49071263+yuyeh1212@users.noreply.github.com>
2025-01-18 10:47:54 +0800

    新增第三周作業

--- w3/vite.config.js
+++ w3/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/React_hw_project/w2/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/React_hw_project/" : "/",
   plugins: [react()],
 });
